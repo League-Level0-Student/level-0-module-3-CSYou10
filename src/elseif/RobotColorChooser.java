12c3fb5b026b936d3574a4e67d43124b811b0198
League Student <league@iMac-93.attlocal.net>
2019-05-20 18:28:10 -0700

    :smile:
    All COMPLETE

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -8,36 +8,43 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
+		// 1. Create a new Robot
 		Robot rob = new Robot();
-		//3. Ask the user what color they would like the robot to draw
-	String color =	JOptionPane.showInputDialog("What color would you like the robot to draw?");
-		
-		//5. Use an if/else statement to set the pen color that the user requested
-if (color .equals("blue")) {
-	rob.setPenColor(Color.blue);
-}else if (color .equals("green")) {
-	rob.setPenColor(Color.green);
-}else if (color .equals("red")) {
-	rob.setPenColor(Color.red);
-}else if (color .equals("yellow")) {
-	rob.setPenColor(Color.yellow);
-}else {
-JOptionPane.showMessageDialog(null, "We can't use that color!");	
-}
-        //6. If the user doesn’t enter anything, choose a random color
+		// 3. Ask the user what color they would like the robot to draw
+		String color = JOptionPane.showInputDialog("What color would you like the robot to draw?");
 
-	
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		// 5. Use an if/else statement to set the pen color that the user requested
 		
-		//4. Set the pen width to 10
+		if (color.equals("blue")) {
+			rob.setPenColor(Color.blue);
+		} else if (color.equals("green")) {
+			rob.setPenColor(Color.green);
+		} else if (color.equals("red")) {
+			rob.setPenColor(Color.red);
+		} else if (color.equals("yellow")) {
+			rob.setPenColor(Color.yellow);
+		} else if (color.equals("orange")) {
+			rob.setPenColor(Color.orange);
+		} else {
+			JOptionPane.showMessageDialog(null, "We can't use that color!");
+		}
+		// 6. If the user doesn’t enter anything, choose a random color
+		Random rand = new Random();
+		new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+		// 7. Put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
+
+		// 4. Set the pen width to 10
 		rob.setPenWidth(10);
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		// 2. Make the robot draw a shape (this will take more than one line of code)
+rob.penDown();
+		rob.setSpeed(18);
+		rob.move(150);
+		rob.move(-250 );
 	}
+	
 }
--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,15 +10,35 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+String	answer =	JOptionPane.showInputDialog("Do you like bananas?");
 		//2. if they say no, 
+if (answer.equals("no")) {
+	JOptionPane.showMessageDialog(null, "You are crazy!");
+	System.exit(0);
+}else if (answer.equals("yes")) {
+	String hobby = JOptionPane.showInputDialog("What is your favorite hobby?");
+	JOptionPane.showMessageDialog(null, hobby +" is much better with bananas!");
+
+	}else if (answer.equals("asdfghjkl;")) {
+JOptionPane.showMessageDialog(null, "You are awesome");
+System.exit(0);
+	}else if (answer.equals("caleb")) {
+	JOptionPane.showMessageDialog(null, "Hi Caleb!");
+	}else if (answer.equals("1234567890")) {
+		JOptionPane.showMessageDialog(null, "You know your numbers");
+	}	
+	else {
+	JOptionPane.showMessageDialog(null, "You are bananas!");
+	System.exit(0);
+}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
+		//4. OPTIONAL: if they say something other than “yes�? or “no�?
+		//	show a pop up that says “You are bananas!�?
 	
 	}
 
